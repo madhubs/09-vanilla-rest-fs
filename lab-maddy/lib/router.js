dba59c7c8a46a06be1d76a5dede2b7619abde7a9
Madeline Stevens <stevensm1987@gmail.com>
2017-08-31 17:40:37 -0700

    scaffolding complete...

--- lab-maddy/lib/router.js
+++ lab-maddy/lib/router.js
@@ -21,44 +21,49 @@ const Router = module.exports = function() {
 
 
 Router.prototype.get = function(endpoint, callback) {
-  debug('#Router.get');
-  this.routes.GET[endpoint] = callback;//callback is line 10
+  debug(`#Router: mounted ${endpoint} GET`);
+  this.routes.GET[endpoint] = callback;
 };
 
 Router.prototype.post = function(endpoint, callback) {
+  debug(`#Router: mounted ${endpoint} POST`);
   this.routes.POST[endpoint] = callback;
 };
 
 Router.prototype.put = function(endpoint, callback) {
+  debug(`#Router: mounted ${endpoint} PUT`);
   this.routes.PUT[endpoint] = callback;
 };
 
 Router.prototype.delete = function(endpoint, callback) {
+  debug(`#Router: mounted ${endpoint} DELETE`);
   this.routes.DELETE[endpoint] = callback;
 };
 
-//this whole callback is what runs when we call http.createServer in server.js
-Router.prototype.route = function(){
-  return(req, res) => {
-    Promise.all([ //we can take in one or many requests. they will be
+Router.prototype.route = function() {
+  return (req, res) => {
+    debug('Someone knows I exist! They have made contact!!');
+    Promise.all([
       parseUrl(req),
       parseJson(req)
     ])
-      .then(()=> {//once all the promises have been returned, we validate that this thing is a function. is the thing on line 10 a valid callback function
-        if(typeof this.routes[req.method][req.url.pathname] === 'function'){
-          this.routes[req.method][req.url.pathname](req, res);
-          return;
-        }
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('route not found');
-        res.end();
-      })
-      .catch(err => {
-        debug(`houston, we have a problem: \n${err.message}`);//used to be consolge.log
+    .then(() => {
+      if(typeof this.routes[req.method][req.url.pathname] === 'function') {
+        debug(`Houston, we've received a request: ${req.url.pathname} ${req.method}`);
+        this.routes[req.method][req.url.pathname](req, res);
+        return;
+      }
 
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.write('bad request; something went wrong in the router');
-        res.end();
-      }); //if everything works properly we'll never hit the .catch
-  };
-};
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.write('route not found');
+      res.end();
+    })
+    .catch(err => {
+      debug(`Houston, we have a problem: \n${err.message}`);
+
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('bad request; something went wrong in the router');
+      res.end() //if everything works properly we'll never hit the .catch
+    })
+  }
+}
