97f4a7184e81bda5daf47072c60929f9f1b66ec0
Madeline Stevens <stevensm1987@gmail.com>
2017-09-10 21:17:10 -0700

    Requests were all working except for DELETE, adjusted response.js and route-toy.js and now PUT won't work.

--- lab-maddy/lib/response.js
+++ lab-maddy/lib/response.js
@@ -2,27 +2,17 @@
 
 const debug = require('debug')('http:response');
 
-let response = module.exports = {};
-
-response.sendJson = function(res, status, data) {
-  debug('no more json');
-  res.writeHead(status, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify(data));
-  res.end();
+module.exports = function(res, status, data) {
+  try {
+    debug('#response sending json');
+    let json = JSON.stringify(data);
+    res.writeHead(status, { 'Content-Type': 'application/json' });
+    res.write(json);
+    res.end();
+  } catch(e) {
+    debug('#response sending text');
+    res.writeHead(status, { 'Content-Type': 'text/plain' });
+    res.write(data);
+    res.end();
+  }
 };
-
-response.sendText = function (res, status, data) {
-  res.writeHead(status, {'Content-Type': 'text/plain'});
-  res.write(data);
-  res.end();
-};
-
-// response.respond = function(res, status, data) {
-//   try (
-//     let json = JSON.stringify(data)
-//     res.writeHead(status, {'Content-Type': 'application/json'})
-//     res.write(json)
-//     res.end()
-//   )
-//
-// }
